a7b28041f7e1e1288559e01acc8464e632afbe55
richardb <none>
2006-05-29 02:35:07 +0200

    Article updates and gel segmentation code
    darcs-hash:20060529003507-fafb9-322bfc10e63dd03b7bfa6b3de09b8a9d4048ce10.gz

--- segGel.cxx
+++ segGel.cxx
@@ -17,6 +17,8 @@
 #include <itkAddImageFilter.h>
 #include <itkGradientRecursiveGaussianImageFilter.h>
 #include <itkGradientToMagnitudeImageFilter.h>
+#include "itkLabelOverlayImageFilter.h"
+#include <itkChangeLabelImageFilter.h>
 
 const int dim = 2;
 
@@ -27,6 +29,9 @@ typedef float FType;
 typedef itk::Image< PType, dim > IType;
 typedef itk::Image< LPType, dim > LIType;
 typedef itk::Image< FType, dim > FIType;
+
+typedef itk::RGBPixel<unsigned char>   RGBPixelType;
+typedef itk::Image<RGBPixelType, dim>    RGBImageType;
 
 
 int main(int arglen, char * argv[])
@@ -52,7 +57,6 @@ int main(int arglen, char * argv[])
   
   medfilt->SetRadius(Sz);
   medfilt->SetInput(reader->GetOutput());
-
   
 #endif
   // find regional minima - do this outside the watershed, because
@@ -102,18 +106,87 @@ int main(int arglen, char * argv[])
   GradFiltType::Pointer grd = GradFiltType::New();
   GradMagType::Pointer grdMag = GradMagType::New();
   grd->SetInput(reader->GetOutput());
-  grd->SetSigma(0.5);
+  grd->SetSigma(0.25);
   grdMag->SetInput(grd->GetOutput());
   // Now apply a watershed
   WSType::Pointer wshed2 = WSType::New();
   wshed2->SetFullyConnected(true);
   wshed2->SetInput(grdMag->GetOutput());
   wshed2->SetMarkerImage(adder->GetOutput());
+  wshed2->SetMarkWatershedLine(false);			
   
-  // file writer
-  typedef itk::ImageFileWriter< LIType > WriterType;
+  // now for the third stage using second derivatives
+  GradFiltType::Pointer grd2 = GradFiltType::New();
+  GradMagType::Pointer grdMag2 = GradMagType::New();
+  grd2->SetInput(grdMag->GetOutput());
+  grd2->SetSigma(0.25);
+  grdMag2->SetInput(grd2->GetOutput());
+
+  // delete the background label
+  typedef itk::ChangeLabelImageFilter<LIType, LIType> ChangeLabType;
+  ChangeLabType::Pointer changer = ChangeLabType::New();
+  changer->SetInput(wshed2->GetOutput());
+  changer->SetChange(labeller->GetObjectCount() + 1, 0);
+  // combine the markers again - could use an eroded version of the
+  // final background marker instead of the original to provide more
+  // constraints
+  AddType::Pointer adder2 = AddType::New();
+  adder2->SetInput1(thresh->GetOutput());
+  adder2->SetInput2(changer->GetOutput());
+  WSType::Pointer wshed3 = WSType::New();
+  wshed3->SetFullyConnected(true);
+  wshed3->SetInput(grdMag2->GetOutput());
+  wshed3->SetMarkerImage(adder2->GetOutput());
+  wshed3->SetMarkWatershedLine(false);			
+
+  // file writers
+  typedef itk::ImageFileWriter< RGBImageType > RGBWriterType;
+  RGBWriterType::Pointer rgbwriter = RGBWriterType::New();
+
+  typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetInput(wshed2->GetOutput());
-  writer->SetFileName("recon.tif");
+
+  // generate output images
+  typedef itk::LabelOverlayImageFilter< IType, LIType, RGBImageType > LabOverlayType;
+  LabOverlayType::Pointer overlay = LabOverlayType::New();
+  // regional minima
+  overlay->SetInput(reader->GetOutput());
+  overlay->SetLabelImage(labeller->GetOutput());
+  overlay->SetBackgroundValue(0);
+  overlay->SetUseBackground(true);
+  overlay->SetOpacity(1.0);
+
+  rgbwriter->SetFileName("gel_minima.png");
+  rgbwriter->SetInput(overlay->GetOutput());
+  rgbwriter->Update();
+  // tesselation
+  overlay->SetLabelImage(thresh->GetOutput());
+  rgbwriter->SetFileName("gel_tesselation.png");
+  rgbwriter->Update();
+  // combined markers
+  overlay->SetLabelImage(adder->GetOutput());
+  rgbwriter->SetFileName("gel_allmarkers.png");
+  rgbwriter->Update();
+  // combined markers 2
+  overlay->SetLabelImage(adder2->GetOutput());
+  rgbwriter->SetFileName("gel_allmarkers2.png");
+  rgbwriter->Update();
+  // result
+  overlay->SetBackgroundValue(labeller->GetObjectCount() + 1);
+  overlay->SetUseBackground(true);
+  overlay->SetOpacity(0.5);
+  overlay->SetLabelImage(wshed2->GetOutput());
+  rgbwriter->SetFileName("gel_result.png");
+  rgbwriter->Update();
+
+  overlay->SetLabelImage(wshed3->GetOutput());
+  rgbwriter->SetFileName("gel_result2.png");
+  rgbwriter->Update();
+  
+  writer->SetInput(grdMag->GetOutput());
+  writer->SetFileName("gel_gradient.png");
+  writer->Update();
+  writer->SetInput(grdMag2->GetOutput());
+  writer->SetFileName("gel_gradient2.png");
   writer->Update();
 }
